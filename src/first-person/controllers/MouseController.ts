7b92897e64832132a086d1aead28ec0b144fe409
Harry Kodua <harry.kodua@rangeforce.com>
2023-07-21 01:37:00 +0300

    chore: pointer lock removed

--- src/first-person/controllers/MouseController.ts
+++ src/first-person/controllers/MouseController.ts
@@ -9,34 +9,17 @@ export class MouseController extends EventTarget {
 
   subscribe() {
     document.addEventListener('mousemove', this.mouseMoveHandler.bind(this));
-    document.addEventListener('mousedown', this.mouseClickHandler.bind(this));
-    document.addEventListener('mouseup', this.mouseReleaseHandler.bind(this));
   }
 
   unsubscribe() {
     document.removeEventListener('mousemove', this.mouseMoveHandler.bind(this));
-    document.removeEventListener('click', this.mouseClickHandler.bind(this));
-    document.removeEventListener('mouseup', this.mouseReleaseHandler.bind(this));
   }
 
   private mouseMoveHandler({ movementY, movementX }: MouseEvent) {
-    // INFO: only updates camera rotation if pointer is locked
-    if (document.pointerLockElement !== document.body) return;
-    // INFO: movement(X|Y) are delta values
     this.camera.rotation.y -= movementX * this.mouseSensitivity;
     this.camera.rotation.x -= movementY * this.mouseSensitivity;
 
     // INFO: clamp camera rotation on X axis
     this.camera.rotation.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, this.camera.rotation.x));
-  }
-
-  private mouseClickHandler() {
-    if (document.pointerLockElement !== null) {
-      this.dispatchEvent(new Event('mouse:click-start'));
-    }
-  }
-
-  private mouseReleaseHandler() {
-    this.dispatchEvent(new Event('mouse:click-end'));
   }
 }
